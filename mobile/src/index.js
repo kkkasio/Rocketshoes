ae1d56a736470cad555d525a0db8238d6ffb4650
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-10-07 09:32:54 -0300

    inicio do mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Hello Wolrd</Text>
+      </SafeAreaView>
+    </>
+  );
+}
